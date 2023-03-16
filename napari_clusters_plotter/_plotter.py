930e65ba7306abb411ea77622954b1ad80cf3668
thorstenwagner <twa1@posteo.de>
2023-03-16 14:50:17 +0100

    make a bit transparent

--- napari_clusters_plotter/_plotter.py
+++ napari_clusters_plotter/_plotter.py
@@ -446,7 +446,7 @@ class PlotterWidget(QMainWindow):
             mask = h2 > output_max
             np.maximum(h2, output_max, out=output_max)
             rgb = [float(v) / 255 for v in list(ImageColor.getcolor(colors[int(cluster) % len(colors)], "RGB"))]
-            rgb.append(1)
+            rgb.append(0.9)
             output[mask] = rgb
 
         return output.swapaxes(0, 1)
